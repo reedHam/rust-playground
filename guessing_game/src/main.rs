bc61a72f9917f1a4027856aec3172444babe7e3e
reedham <reed.hambrook@gmail.com>
2021-05-08 16:33:32 -0700

    change: reduced the total range for secret numbers

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,7 +6,7 @@ use rand::Rng;
 fn main() {
     println!("Guess your number!");
     
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..=10);
 
     println!("The secret number is: {}", secret_number);
 
