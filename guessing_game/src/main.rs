a2a3c6577467eeeec76cd12cb560946a31adf7dc
reedham <reed.hambrook@gmail.com>
2021-05-08 15:50:33 -0700

    add: random number is now generated

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,14 @@
 // Bring the standard io module into scope
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess your number!");
     
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     // Create a new instance of the String type
