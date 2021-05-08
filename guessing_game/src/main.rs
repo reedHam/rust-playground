676c98215b590d940a8f43d42c893b846484b6c5
reedham <reed.hambrook@gmail.com>
2021-05-08 16:09:24 -0700

    add: can now compare guess against secret

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 // Bring the standard io module into scope
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -21,7 +22,18 @@ fn main() {
         // Since read_line returns a Result with an Err variant it must be handled with the expect
         .expect("Failed to read line.");
 
+    // Due to the new line captured by read line guess is a string 
+    // to convert guess into a number we trim and parse it while specifying the new data type u32
+    let guess: u32 = guess.trim().parse().expect("Please type a number.");
+
     // String templates are possible with println 
     // Placeholders are replaced in left to right order with more than one variable
     println!("You guessed: {}", guess);
+
+    // Match compares a value to keys with code branches
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => println!("You got it!"),
+        Ordering::Greater => println!("Too big!"),
+    }
 }
