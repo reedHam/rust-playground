9bdaca1dadef4ae42652ef8014252bb219b3838b
reedham <reed.hambrook@gmail.com>
2021-05-08 16:21:31 -0700

    add: now loops untill a correct guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,30 +10,35 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // Create a new instance of the String type
-    let mut guess = String::new();
+        // Create a new instance of the String type
+        let mut guess = String::new();
 
-    // Call stdin from the io module
-    io::stdin()
-        // &mut specifies that guess is a mutable reference 
-        .read_line(&mut guess)
-        // Since read_line returns a Result with an Err variant it must be handled with the expect
-        .expect("Failed to read line.");
+        // Call stdin from the io module
+        io::stdin()
+            // &mut specifies that guess is a mutable reference 
+            .read_line(&mut guess)
+            // Since read_line returns a Result with an Err variant it must be handled with the expect
+            .expect("Failed to read line.");
 
-    // Due to the new line captured by read line guess is a string 
-    // to convert guess into a number we trim and parse it while specifying the new data type u32
-    let guess: u32 = guess.trim().parse().expect("Please type a number.");
+        // Due to the new line captured by read line guess is a string 
+        // to convert guess into a number we trim and parse it while specifying the new data type u32
+        let guess: u32 = guess.trim().parse().expect("Please type a number.");
 
-    // String templates are possible with println 
-    // Placeholders are replaced in left to right order with more than one variable
-    println!("You guessed: {}", guess);
+        // String templates are possible with println 
+        // Placeholders are replaced in left to right order with more than one variable
+        println!("You guessed: {}", guess);
 
-    // Match compares a value to keys with code branches
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You got it!"),
-        Ordering::Greater => println!("Too big!"),
+        // Match compares a value to keys with code branches
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => { 
+                println!("You got it!");
+                break;
+            },
+            Ordering::Greater => println!("Too big!"),
+        }
     }
 }
