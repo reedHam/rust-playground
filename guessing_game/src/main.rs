2362772a2d4faaa5e02e94ab4715eeb1a7900d80
reedham <reed.hambrook@gmail.com>
2021-05-08 13:18:02 -0700

    change: added a prompt to the program

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,16 @@
+// Bring the standard io library into scope
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess your number!");
+    
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    println!("You guessed: {}", guess);
 }
