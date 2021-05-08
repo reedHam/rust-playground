a4d4d15aebc08c992bf4780e5d188ce67db51609
reedham <reed.hambrook@gmail.com>
2021-05-08 12:04:54 -0700

    change: added comments to main.rs

--- hello-world/main.rs
+++ hello-world/main.rs
@@ -1,3 +1,6 @@
+// the main function is the entry point for every rust executable
 fn main() {
+    // println! is a rust macro
+    // the ! is what designates a call to macros
     println!("Hello, world!");
 }
