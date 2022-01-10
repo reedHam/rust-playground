99dcfff91d9c5eb9f54933bc85299616c7d87713
reedHam <reed.hambrook@gmail.com>
2022-01-10 14:05:01 -0800

    added error checking to month indexer

--- array-index/src/main.rs
+++ array-index/src/main.rs
@@ -1,20 +1,30 @@
 use std::io;
 
+// Define 12 pointers on the stack to strings in the heap
+const MONTH_ARRAY: [&str; 12] = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+fn get_month(index: u8) -> Result<&'static str, &'static str> {
+    if index == 0 || index > 12 {
+        return Err("Invalid month index");
+    }
+
+    return Ok(MONTH_ARRAY[index as usize - 1]);
+}
+
 fn main() {
-    const MONTH_ARRAY: [&str; 12] = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
     loop {
         println!("Enter a number between 1 and 12 to get the month represented by that number:");
         let mut index = String::new();
@@ -30,11 +40,13 @@ fn main() {
             }
         };
 
-        if index == 0 || index > 12 {
-            println!("Invalid Input.");
-            continue;
-        }
-
-        println!("{}", MONTH_ARRAY[index as usize - 1]);
+        let month = match get_month(index) {
+            Ok(month) => month,
+            Err(err) => {
+                println!("{}", err);
+                continue;
+            }
+        };
+        println!("{}", month);
     }
 }
