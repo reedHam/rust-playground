732173680d2023b711d784cc188917e8fce24101
Reed Hambrook <reed.hambrook@gmail.com>
2022-04-02 13:18:22 -0700

    added unsafe examples

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,4 +1,4 @@
-fn appendQuestionMark(s: &mut String) {
+fn append_question_mark(s: &mut String) {
     s.push_str("?");
 }
 
@@ -37,7 +37,44 @@ fn main() {
     let mut s5 = String::from("What");
     // Append question mark will borrow s5 and mutate it
     // because s5 is a mutable reference we after the function s5 is not dropped
-    appendQuestionMark(&mut s5);
+    append_question_mark(&mut s5);
     // here s5 is owned by s5 again
     println!("s5: {}", s5); // What?
+
+    let mut str_vec = vec![String::from("hello"), String::from("world")];
+    // Here we create a vector of strings on the heap
+    println!("str_vec: {:?}", str_vec); // ["hello", "world"]
+
+    // since you cannot borrow a variable more than once at a time
+    // we cant swap elements in the vector
+    // e.g.
+    // let s = str_vec[0];
+    // let t = str_vec[1];
+    // str_vec[0] = t;
+    // str_vec[1] = s;
+    // This will not compile since we cannot borrow str_vec more than once at a time
+    // to accomplish this we need to use unsafe code,
+    unsafe {
+        // Here we get raw pointers to the first and second elements of the vector
+        let ptr_a: *mut String = &mut str_vec[0];
+        let ptr_b: *mut String = &mut str_vec[1];
+        // we then call the pointer swap function
+        std::ptr::swap(ptr_a, ptr_b);
+    }
+    println!("str_vec: {:?}", str_vec); // ["world", "hello"]
+
+    // there is a function that encapsulates the swap function
+    str_vec.swap(0, 1);
+    println!("str_vec: {:?}", str_vec); // ["hello", "world"]
+
+    let mut str_vec1 = vec![String::from("hello"), String::from("world")];
+    let mut str_vec2 = vec![String::from("hi"), String::from("mom")];
+    // Here we create two vectors of references to strings
+    println!("str_vec1: {:?}", str_vec1); // ["hello", "world"]
+    println!("str_vec2: {:?}", str_vec2); // ["hi", "mom"]
+
+    // now we will preform a memory swap
+    std::mem::swap(&mut str_vec1[0], &mut str_vec2[0]); // also uses unsafe code
+    println!("str_vec1: {:?}", str_vec1); // ["hi", "world"]
+    println!("str_vec2: {:?}", str_vec2); // ["hello", "mom"]
 }
